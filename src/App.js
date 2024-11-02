21e8738a77c45c93d2530151f550b160f3bea312
mtmodesti <marcelomodesti1988@gmail.com>
2024-11-02 10:28:33 -0300

    windoiws

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <span>initial commit</span>
+      <span>initial commit 2 </span>
+      <span>initial commit 3 </span>
     </div>
   );
 }
