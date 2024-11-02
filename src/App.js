53d035a446e0e1606fa2d553eb488d216b0edd57
mtmodesti <marcelomodesti1988@gmail.com>
2024-11-02 10:29:51 -0300

    Atualiza a lógica de renderização no App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
       <span>initial commit</span>
       <span>initial commit 2 </span>
       <span>initial commit 3 </span>
+      <span>initial commit 4 </span>
+      <span>initial commit 5 </span>
+      <span>initial commit 6 </span>
     </div>
   );
 }
