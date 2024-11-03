eea06361625b832fd5fc02a22039084842f56fd1
mtmodesti <marcelomodesti1988@gmail.com>
2024-11-03 08:33:17 -0300

    feat: login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
+import "./styles/styles.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <span>initial commit</span>
-      <span>initial commit 2 </span>
-      <span>initial commit 3 </span>
-      <span>initial commit 4 </span>
-      <span>initial commit 5 </span>
-      <span>initial commit 6 </span>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import LoginForm from "../../components/loginForm"; // Ajuste o caminho
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+
+export default Home;
